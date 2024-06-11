04ebebdbd78c7249e666e2bc3eb58c1809333a24
Dozz Technology <dozz@MacBook-Air-de-Dozz.local>
2024-06-11 13:08:30 -0300

    Proxy 2

--- proxy.js
+++ proxy.js
@@ -10,7 +10,7 @@ app.get('/recording', async (req, res) => {
   const { recordingPath } = req.query;
 
   if (!recordingPath) {
-    return res.status(400).send('Parâmetros insuficientes');
+    return res.status(400).send('Parâmetros insuficientes'); 
   }
 
   try {
