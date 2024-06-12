f2014d460e8fb08bd5059dd388e2fd0a7c2d1db5
Dozz Technology <dozz@MacBook-Air-de-Dozz.local>
2024-06-12 02:28:03 -0300

    S3 13

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 5010;
+
+app.get('/filepath/:filePath', (req, res) => {
+    const { filePath } = reque.params;
+    console.log('filePath: ', filePath)
+  res.status(200).json({ message: 'Processing the file' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
