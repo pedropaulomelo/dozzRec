e1ab0a72ff3a5be0ee2ebda2bb1935114c3e3327
Dozz Technology <dozz@MacBook-Air-de-Dozz.local>
2024-06-12 00:23:49 -0300

    S3 8

--- sendnode.js
+++ sendnode.js
@@ -5,9 +5,6 @@ const fs = require('fs');
 // URL do endpoint do servidor Flask
 const url = 'https://8281e92c1458.ngrok.app/transcribe';
 
-// Caminho do arquivo de áudio
-const filePath = '/Users/dozz/Downloads/q-150-5031-20240611-171657-1718137017.2015830.wav';
-
 // Função para enviar o arquivo de áudio
 exports.sendAudioFile = async (filePath) => {
     const form = new FormData();
@@ -23,6 +20,3 @@ exports.sendAudioFile = async (filePath) => {
         console.error('Erro ao enviar arquivo:', error.message);
     }
 }
-
-// Enviar o arquivo e obter a resposta
-sendAudioFile(filePath);
