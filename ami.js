7e7fdb1bd05f8c3ea508614c3a2e8ee4dbf1c165
Dozz Technology <dozz@MacBook-Air-de-Dozz.local>
2024-06-12 03:18:15 -0300

    S3 17

---
+++ ami.js
@@ -0,0 +1,46 @@
+const ami = require('asterisk-manager');
+const { uploadToS3 } = require('./upload');
+
+// Configuração de conexão
+
+const ip = 'http://localhost';
+// const ip = 'portermaringa.dyndns.org';
+const port = 5038;  // Porta padrão do AMI
+const user = 'dozz';
+const secret = 'SgAlbCahwTDaxJtonP5X6QgD9olWIdPT';
+ 
+const amiConnection = new ami(port, ip, user, secret, true);
+
+console.log('Cloud Voice AMI connection started');
+
+const recordingFiles = {};
+
+amiConnection.on('response', (res) => {
+    console.error('Connection:', res);
+});
+
+amiConnection.on('managerevent', (evt) => {
+    // console.log(evt)
+
+    if (evt.event === 'VarSet' && evt.variable.MIXMONITOR_FILENAME === '') {
+        // console.log(evt)
+        const callId = evt.uniqueid;
+        const recordingPath = evt.value;
+        recordingFiles[callId] = recordingPath;
+    }
+
+    if  (evt.event === 'BridgeLeave' && evt.bridgenumchannels === '0' && evt.context === 'macro-dial-one') {
+        // console.log(evt)
+        const from = evt.calleridnum;
+        const to = evt.connectedlinenum;
+        const callId = evt.linkedid;
+
+        console.log(`Ligação de ${from} para ${to} foi encerrada - ID: ${callId}`);
+
+        const recordingFile = recordingFiles[callId];
+        delete recordingFiles[callId];
+
+        console.log('Sending the filePath to the voice server: ', recordingFile)
+        uploadToS3(recordingFile);
+    }
+});
---
+++ upload.js
@@ -0,0 +1,37 @@
+const AWS = require('aws-sdk');
+const path = require('path');
+const fs = require('fs');
+require('dotenv').config();
+const { sendAudioFile } = require('./sendnode');
+
+// Configurar as credenciais da AWS
+AWS.config.update({
+    accessKeyId: process.env.BUCKET_ACCESS_KEY,
+    secretAccessKey: process.env.BUCKET_SECRET_ACCESS_KEY,
+    region: process.env.BUCKET_REGION
+});
+
+const s3 = new AWS.S3();
+const bucketName = process.env.BUCKET_NAME;
+
+// Função para fazer upload dos arquivos para o S3
+exports.uploadToS3 = (filePath) => {
+    const fileName = path.basename(filePath);
+    const fileStream = fs.createReadStream(filePath);
+
+    const params = {
+        Bucket: bucketName,
+        Key: fileName,
+        Body: fileStream
+    };
+
+    s3.upload(params, async (err, data) => {
+        if (err) {
+            console.log(`Failed to upload ${fileName}:`, err);
+        } else {
+            console.log(`Uploaded ${fileName} to ${bucketName}`);
+            const response = await sendAudioFile(filePath);
+            console.log('TRANSCRIPTION: ', response);
+        }
+    });
+};
