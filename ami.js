a525516de11408702b8a7ca24761faa8e4541dc3
Dozz Technology <dozz@MacBook-Air-de-Dozz.local>
2024-06-13 10:08:10 -0300

    Diferentiate in and out calls

--- ami.js
+++ ami.js
@@ -3,7 +3,8 @@ const { uploadToS3 } = require('./upload');
 
 // Configuração de conexão
 
-const ip = '127.0.0.1';
+const ip = '18.230.133.246';
+// const ip = '127.0.0.1';
 // const ip = 'portermaringa.dyndns.org';
 const port = 5038;  // Porta padrão do AMI
 const user = 'dozz';
@@ -22,13 +23,31 @@ amiConnection.on('response', (res) => {
 amiConnection.on('managerevent', (evt) => {
     // console.log(evt)
 
+    if (evt.event === 'VarSet' && evt.calleridnum === evt.connectedlinenum && evt.variable.MIXMONITOR_FILENAME === '') {
+        // console.log(evt)
+        const callId = evt.uniqueid;
+        const from = evt.calleridnum;
+        const recordingPath = evt.value;
+        recordingFiles[callId] = [recordingPath, false];
+        console.log('OUT CALL START: ', callId, from );
+    }
+
+    if (evt.event === 'VarSet' && evt.calleridnum !== evt.connectedlinenum && evt.variable.MIXMONITOR_FILENAME === '') {
+        // console.log(evt)
+        const callId = evt.uniqueid;
+        const from = evt.calleridnum;
+        const recordingPath = evt.value;
+        recordingFiles[callId] = [recordingPath, true];
+        console.log('IN CALL START: ', callId, from );
+    }
+
     if (evt.event === 'VarSet' && evt.variable.MIXMONITOR_FILENAME === '') {
         // console.log(evt)
         const callId = evt.uniqueid;
         const from = evt.calleridnum;
         const recordingPath = evt.value;
         recordingFiles[callId] = recordingPath;
-        console.log('CALL START: ', callId, 'FROM: ', from );
+        console.log('IN CALL START: ', callId, from );
     }
 
     if  (evt.event === 'BridgeLeave' && evt.bridgenumchannels === '0') {
@@ -36,16 +55,11 @@ amiConnection.on('managerevent', (evt) => {
         const from = evt.calleridnum;
         const to = evt.connectedlinenum;
         const callId = evt.linkedid;
-
-        // console.log(`Ligação de ${from} para ${to} foi encerrada - ID: ${callId}`);
-        console.log('CALL END: ', callId, 'FROM: ', from, 'TO: ', to);
-
-        const recordingFile = recordingFiles[callId];
-
-        console.log('RECORDING FILE: ', recordingFile)
+        const recordingFile = recordingFiles[callId][0];
+        const inCall = recordingFiles[callId][1];
+        const logTxt = inCall ? 'IN CALL END: ' : 'OU CALL END: ';
         delete recordingFiles[callId];
-
-        // console.log('Sending the filePath to the voice server: ', recordingFile)
+        recordingFile && console.log(logTxt, callId, from, recordingFile);
         recordingFile &&  uploadToS3(recordingFile);
     }
 });
